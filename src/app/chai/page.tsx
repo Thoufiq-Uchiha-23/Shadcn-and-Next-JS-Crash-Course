4f5a4a918f34e737a58730ade48f786f825401c3
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-07-06 22:03:39 +0530

    chai

--- src/app/chai/page.tsx
+++ src/app/chai/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 const ChaiPage = () => {
   return (
     <main className="h-full flex justify-center items-center flex-col">
         <div>ChaiPage 😄</div>
         <button className="px-4 py-2 bg-blue-500 rounded-md my-3">Test button</button>
+        <Button>Shadcn Button</Button>
     </main>
   )
 }
